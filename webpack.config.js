17ec8cf8f108750e0e4d82149311fb8e9cec5fd7
Cuong Nguyen <littlecuong922@gmail.com>
2024-05-24 13:49:11 +0700

    Change public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (_, argv) => ({
   output: {
+    // local
     publicPath: "http://localhost:8080/",
+
+    // deployed
+    publicPath: "https://main.d219oo8bkd6xcl.amplifyapp.com/",
   },
 
   resolve: {
