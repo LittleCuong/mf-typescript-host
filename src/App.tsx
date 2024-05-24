fc9038bb100674cea1e937af07db679ece2c690d
Cuong Nguyen <littlecuong922@gmail.com>
2024-05-24 13:57:51 +0700

    Add authentication with Cognito

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+
+import outputs from '../amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+
 import Remote from "remote/Recipes"
 
 import './index.scss'
 
+Amplify.configure(outputs);
+
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: host</div>
-    <Remote/>
-  </div>
+  <Authenticator>
+      {({ signOut, user }) => (  
+        <div className="mt-10 text-3xl mx-auto max-w-6xl">
+          <Remote/>
+        </div>
+      )}
+    </Authenticator>
+
 )
 const rootElement = document.getElementById('app')
 if (!rootElement) throw new Error('Failed to find the root element')
