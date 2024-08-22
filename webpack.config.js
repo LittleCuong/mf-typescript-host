cd2ad0a6484f1d48eefb744e3c90b59667ec00fe
Geralt <littlecuong@gmail.com>
2024-08-22 11:29:51 +0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -11,7 +11,7 @@ module.exports = (_, argv) => ({
     // publicPath: "http://localhost:8080/",
 
     // deployed
-    publicPath: "https://main.d2h4kex1uqiz9e.amplifyapp.com/",
+    publicPath: "https://main.d1dve9oky2zsoy.amplifyapp.com/",
   },
 
   resolve: {
@@ -43,19 +43,21 @@ module.exports = (_, argv) => ({
           loader: "babel-loader",
         },
       },
-      { test: /\.json$/, type: 'json' },
+      { test: /\.json$/, type: "json" },
     ],
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].contenthash.css"
+      filename: "[name].contenthash.css",
     }),
     new ModuleFederationPlugin({
       name: "host",
       filename: "remoteEntry.js",
       remotes: {
-        remote: "remote@https://main.dl71rq11tiul9.amplifyapp.com/remoteEntry.js"
+        remote:
+          "remote@https://main.d1prd2j1vamtcr.amplifyapp.com/remoteEntry.js", // deploy
+        // "remote@http://localhost:8081/remoteEntry.js",
       },
       exposes: {},
       shared: {
@@ -73,6 +75,6 @@ module.exports = (_, argv) => ({
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
 });
